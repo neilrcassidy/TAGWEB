03dbf112bc17fa84e7fa599a4b26752dbf556b49
neilrcassidy <neilrobertcassidy@gmail.com>
2024-02-12 10:30:20 +0100

    fixed update logos for inactive events

--- src/components/EventInactiveCardGrid.jsx
+++ src/components/EventInactiveCardGrid.jsx
@@ -12,8 +12,8 @@ import 'tippy.js/dist/tippy.css';
 
 const EventInactiveCardGrid = ({ userBadges, title, category, emoji }) => {
   return (
-    <div id={"badges"+title} className={`flex flex-col rounded-lg border border-gray-400 max-w-[350px] w-[95%]`}>
-      <div id={"badges"+title+"Title"} className={`flex bg-gray-400 rounded-t-md text-white`}>
+    <div id={"badges" + title} className={`flex flex-col rounded-lg border border-gray-400 max-w-[350px] w-[95%]`}>
+      <div id={"badges" + title + "Title"} className={`flex bg-gray-400 rounded-t-md text-white`}>
         <div className={`m-auto my-2 ml-3 text-[20px]`}>
           <h3>Evento: {title} {emoji}</h3>
         </div>
@@ -21,7 +21,7 @@ const EventInactiveCardGrid = ({ userBadges, title, category, emoji }) => {
           <h3>{badges.filter((badge) => userBadges.includes(badge.id) && badge.group === category).length}/{badges.filter((badge) => badge.group === category).length}</h3>
         </div>
       </div>
-      <div id={"badges"+category+"Grid"} className={`mx-1 my-4`}>
+      <div id={"badges" + category + "Grid"} className={`mx-1 my-4`}>
         <div className={`flex flex-wrap ${styles.flexCenter} gap-3`}>
           {badges
             .filter((badge) => badge.group === category)
@@ -29,8 +29,10 @@ const EventInactiveCardGrid = ({ userBadges, title, category, emoji }) => {
               <Tippy content=
                 {<div className={`flex flex-row gap-4 m-2`}>
                   <div className={`flex m-auto max-w-[128px] min-w-[128px]`}>
-                    <img className={`${badge.type === "rare" ? "glow-rare-badges" : ""}`} src={userBadges.includes(badge.id) ? badge.icon_unlocked : badge.icon_locked}></img>
-                    <img className={`absolute top-[80px] left-[80px] w-[58px]`} src={badge.updateIcon} />
+                    <div className={`relative top-0 left-0`}>
+                      <img className={`${badge.type === "rare" ? "glow-rare-badges" : ""}`} src={userBadges.includes(badge.id) ? badge.icon_unlocked : badge.icon_locked}></img>
+                      <img className={`absolute top-[80px] left-[80px] w-[58px]`} src={badge.updateIcon} />
+                    </div>
                   </div>
                   <div className={`flex flex-col font-poppins text-left m-auto`}>
                     <div className={`font-bold text-[18px]`}>
@@ -43,8 +45,10 @@ const EventInactiveCardGrid = ({ userBadges, title, category, emoji }) => {
                 </div>}>
                 <div id={badge.id} className={`flex flex-col`}>
                   <div className={`${styles.flexCenter} w-[96px] h-[86px]`}>
-                    <img className={`${styles.flexCenter} w-[72px] ${badge.type === "rare" ? "glow-rare-badges" : ""}`} src={userBadges.includes(badge.id) ? badge.icon_unlocked : badge.icon_locked}></img>
-                    <img className={`absolute top-[44px] left-[44px] w-[36px]`} src={badge.updateIcon} />
+                    <div className={`relative top-0 left-0`}>
+                      <img className={`${styles.flexCenter} w-[72px] ${badge.type === "rare" ? "glow-rare-badges" : ""}`} src={userBadges.includes(badge.id) ? badge.icon_unlocked : badge.icon_locked}></img>
+                      <img className={`absolute top-[44px] left-[44px] w-[36px]`} src={badge.updateIcon} />
+                    </div>
                   </div>
                   <div className={`${styles.flexCenter} font-normal`}>
                     <img src={antxpoint} className={`w-[18px] mr-1 m-auto ml-0`} />
