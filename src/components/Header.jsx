978ebc1204c1ff37d53b8dd83f3667ef93c5b5db
neilrcassidy <neilrobertcassidy@gmail.com>
2024-06-15 16:09:30 +0200

    updated version in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = ({ isMobileMenuToggled, toggleMenu, isUserLogged }) => {
       <div className={`flex m-auto xs:ml-12 ml-6 font-poppins gap-1`}>
         <img src={headerTitle} className="h-auto xs:w-[110px] w-[80px]" onClick={() => navBadges()}/>
         <div className={`flex`}>
-          <p className={`flex items-end`}>v1.2.1</p>
+          <p className={`flex items-end`}>v1.2.2</p>
         </div>
         
       </div>
